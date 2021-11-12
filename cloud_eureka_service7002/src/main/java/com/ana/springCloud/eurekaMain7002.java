20719d9ee423e7349610811257b6dedcf011499b
DawnTaTa <m18681496361@163.com>
2021-11-12 17:23:10 +0800

    eureka

--- cloud_eureka_service7002/src/main/java/com/ana/springCloud/eurekaMain7002.java
+++ cloud_eureka_service7002/src/main/java/com/ana/springCloud/eurekaMain7002.java
@@ -2,14 +2,14 @@ package com.ana.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain {
+@EnableEurekaServer
+public class eurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class,args);
+        SpringApplication.run(eurekaMain7002.class,args);
     }
 
 }
