8fae775d99429330046aab2f2b001c53aa765fca
DawnTaTa <m18681496361@163.com>
2021-11-25 15:35:53 +0800

    Hystrix

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+
+    //获取时区串
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+
+}
--- cloud_payment_consumer/src/main/java/com/ana/springCloud/config/ApplicationContextConfig.java
+++ cloud_payment_consumer/src/main/java/com/ana/springCloud/config/ApplicationContextConfig.java
@@ -1,6 +1,5 @@
-package com.ana.springCloud.config;
+package com.ana.springcloud.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
